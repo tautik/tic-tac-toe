8c56cc87cf44c3d31029122e2dc4657433a72374
Tautik Agrahari <tautik@192.168.1.3>
2022-12-22 15:26:49 +0530

    first commit

---
+++ src/Components/Box.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "../Styles/Box.css";
+export default function Box(props) {
+  let [input, setInput] = useState(" ");
+
+  let condition = {
+    row: [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ],
+    column: [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ],
+    diagonal: [
+      [0, 4, 8],
+      [2, 4, 6],
+    ],
+  };
+  let checkResult = (boxes) => {
+    for (let index in condition) {
+      condition[index].forEach((x) => {
+        console.log(x[0]);
+        if (boxes[x[0]] == " " || boxes[x[1]] == " " || boxes[x[2]] == " ") {
+          console.log("Not wone yet");
+        } else if (boxes[x[0]] == boxes[x[1]] && boxes[x[1]] == boxes[x[2]]) {
+          props.setWinner(boxes[x[0]] == "X" ? 1 : 0);
+        }
+      });
+    }
+  };
+  function register() {
+    let boxes = { ...props.status };
+    if (input == " ") {
+      if (props.player == 1) {
+        setInput("X");
+        boxes[props.num] = "X";
+        props.setPlayer(2);
+      } else {
+        setInput("0");
+        boxes[props.num] = "O";
+        props.setPlayer(1);
+      }
+    } else {
+      alert("Again clicking wont work buddy");
+    }
+    props.setStatus(boxes);
+    console.log(boxes);
+    checkResult(boxes);
+  }
+
+  return <td onClick={register}>{input}</td>;
+}
---
+++ src/Components/Main.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+
+import Box from "./Box";
+import "../Styles/Main.css";
+function Main() {
+  let [player, setPlayer] = useState(1);
+  let [status, setStatus] = useState(Array(9).fill(" "));
+  let [winner, setWinner] = useState();
+  function turn() {
+    if (player == 1) return 1;
+    else return 2;
+  }
+  function winStatus() {
+    if (winner == `None`) return winner;
+    else if (winner == 1) return `Player 1`;
+    else return `Player 2`;
+  }
+  return (
+    <main>
+      <h1>Tic Tac Toe</h1>
+      <div className="turn-container">
+        <h3>Player {player} turn</h3>
+      </div>
+      <div className="Container">
+        <table>
+          <tr>
+            <Box
+              setWinner={setWinner}
+              status={status}
+              setStatus={setStatus}
+              player={player}
+              setPlayer={setPlayer}
+              num={0}
+            />
+            <Box
+              setWinner={setWinner}
+              status={status}
+              setStatus={setStatus}
+              player={player}
+              setPlayer={setPlayer}
+              num={1}
+            />
+            <Box
+              setWinner={setWinner}
+              status={status}
+              setStatus={setStatus}
+              player={player}
+              setPlayer={setPlayer}
+              num={2}
+            />
+          </tr>
+          <tr>
+            <Box
+              setWinner={setWinner}
+              status={status}
+              setStatus={setStatus}
+              player={player}
+              setPlayer={setPlayer}
+              num={3}
+            />
+            <Box
+              setWinner={setWinner}
+              status={status}
+              setStatus={setStatus}
+              player={player}
+              setPlayer={setPlayer}
+              num={4}
+            />
+            <Box
+              setWinner={setWinner}
+              status={status}
+              setStatus={setStatus}
+              player={player}
+              setPlayer={setPlayer}
+              num={5}
+            />
+          </tr>
+          <tr>
+            <Box
+              setWinner={setWinner}
+              status={status}
+              setStatus={setStatus}
+              player={player}
+              setPlayer={setPlayer}
+              num={6}
+            />
+            <Box
+              setWinner={setWinner}
+              status={status}
+              setStatus={setStatus}
+              player={player}
+              setPlayer={setPlayer}
+              num={7}
+            />
+            <Box
+              setWinner={setWinner}
+              status={status}
+              setStatus={setStatus}
+              player={player}
+              setPlayer={setPlayer}
+              num={8}
+            />
+          </tr>
+        </table>
+      </div>
+      <div>
+        {winner && (
+          <div className="winner-section">
+            <h3 className="winner-text">Winner is player {winner}</h3>
+            <button>Reset</button>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
+
+export default Main;
